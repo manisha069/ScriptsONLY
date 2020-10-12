cbe7372cfa65d0f5d5ae48767d88ed6ee09d6fbc
dhhruv <180320107529.ce.dhruv@gmail.com>
2020-10-12 19:48:17 +0530

    MatrixSpiral.py Updated

--- python/MatrixSpiral.py
+++ python/MatrixSpiral.py
@@ -7,27 +7,11 @@ for example: input ->> [[1,2,3],
 
 
 class Solution:
-    def matrix_spiral(self, matrix):
-        """
-        :type matrix: list[list[]]
-        """
-
-        starting_row = 0
-        ending_row = len(matrix)
-        starting_col = 0
-        ending_col = len(matrix[0])
-        while starting_row < ending_row and starting_col < ending_col:
-            for k in range(starting_col, ending_col):
-                print(matrix[starting_row][k], end=" ")
-            starting_row += 1
-            for k in range(starting_row, ending_row):
-                print(matrix[k][ending_col-1], end=" ")
-            ending_col -= 1
-            if starting_row < ending_row:
-                for k in range(ending_col-1, starting_col-1, -1):
-                    print(matrix[ending_row-1][k], end=" ")
-                ending_row -= 1
-            if starting_col < ending_col:
-                for k in range(ending_row-1, starting_row-1, -1):
-                    print(matrix[k][starting_col], end=" ")
-                starting_col += 1
+    def solve(self, matrix):
+        ls=list()
+        while matrix:
+            ls.extend(matrix[0])
+            #print(ls)
+            matrix=list(zip(*matrix[1:]))[::-1]
+            #print(matrix)
+        return ls
